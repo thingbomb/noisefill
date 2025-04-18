1eef70069252d7b57da7e383519ae59bdc84783f
georg-stone <g3eorge@pm.me>
2025-04-17 20:06:48 -0400

    better sidebar button spacing

--- src/components/ui/sidebar.jsx
+++ src/components/ui/sidebar.jsx
@@ -453,7 +453,7 @@ const SidebarMenu = React.forwardRef(({ className, ...props }, ref) => (
   <ul
     ref={ref}
     data-sidebar="menu"
-    className={cn("flex w-full min-w-0 flex-col gap-1", className)}
+    className={cn("flex w-full min-w-0 flex-col gap-[1px]", className)}
     {...props}
   />
 ));
